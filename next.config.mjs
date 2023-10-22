f4bf6bb77af65251d670fcb61cc461f0c2fdd785
Noah Kurrack <noahkurrack@gmail.com>
2023-10-22 16:19:12 -0400

    Fix code execution on Vercel (#2)
    * update packages, move sandbox to upstart
    
    * fix with package-patch

--- next.config.mjs
+++ next.config.mjs
@@ -10,12 +10,6 @@ const config = {
   images: {
     domains: ["images.clerk.dev"],
   },
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
