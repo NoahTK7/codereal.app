634cd4c94c6c8b3ab7b254ced9962b72d39f5e19
Noah Kurrack <noahkurrack@gmail.com>
2024-06-20 21:04:15 -0400

    finish setting up feature toggle infra

--- src/server/api/routers/state.ts
+++ src/server/api/routers/state.ts
@@ -1,6 +1,7 @@
 import { filterQuestionForClientPublic } from "~/server/helpers/filter";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { getLatestQuestion } from "./question";
+import { type PrismaClient } from "@prisma/client";
 
 export type ClientFeatureToggleState = Record<string, boolean>
 export type ServerFeatureToggleState = Record<string, boolean>
@@ -12,19 +13,33 @@ export const globalStateRouter = createTRPCRouter({
       return filterQuestionForClientPublic(latestQuestion)
     }),
   getFeatureToggles: publicProcedure
-    .query(({ }) => {
-      return getClientFeatureToggles()
+    .query(async ({ ctx }) => {
+      return await getClientFeatureToggles(ctx.db)
     }),
 });
 
-const getClientFeatureToggles = (): ClientFeatureToggleState => {
-  return {
-    allowMultipleAttempts: true
+const getClientFeatureToggles = async (db: PrismaClient): Promise<ClientFeatureToggleState> => {
+  const records = await db.featureToggle.findMany({
+    where: {
+      type: "CLIENT"
+    }
+  })
+  const out = {} as ClientFeatureToggleState
+  for (const record of records) {
+    out[record.key] = record.value
   }
+  return out
 }
 
-const getServerFeatureToggles = (): ServerFeatureToggleState => {
-  return {
-    allowMultipleAttempts: true
+const getServerFeatureToggles = async (db: PrismaClient): Promise<ServerFeatureToggleState> => {
+  const records = await db.featureToggle.findMany({
+    where: {
+      type: "SERVER"
+    }
+  })
+  const out = {} as ClientFeatureToggleState
+  for (const record of records) {
+    out[record.key] = record.value
   }
+  return out
 }
